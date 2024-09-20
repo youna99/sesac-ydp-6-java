7a80c025efecf21a0e405721c06beb79d1c6c4c8
youna <dbsk0108@gmail.com>
2024-09-21 00:42:52 +0900

    IntelliJ 연결 복습2

--- SeSAC.test/src/Main.java
+++ SeSAC.test/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello, java");
+        System.out.println("Hello, java and  IntelliJ");
     }
 }
