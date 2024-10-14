46e900eccfd85013ee2f4af2a0f562f6f2fd8e9d
youna99 <dbsk0108@gmail.com>
2024-10-14 18:45:57 +0900

    study: builder 디자인 패턴, JPA

--- java-language/src/_10_others/_builder/Computer.java
+++ java-language/src/_10_others/_builder/Computer.java
@@ -1,4 +1,65 @@
 package _10_others._builder;
 
+
 public class Computer {
+    // 모든 필드를 private final 선언 (불변성 보장)
+    private final String cpu;
+    private final int ram;
+    private final int storage;
+    private final String gpu;
+
+    // private 생성자: 외부에서 직접 호출 불가능
+    private Computer(ComputerBuilder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.storage = builder.storage;
+        this.gpu = builder.gpu;
+    }
+
+    // 내부 정적 클래스: public 선언 -> 외부에서 접근 가능
+    public static class ComputerBuilder {
+        private String cpu;
+        private int ram;
+        private int storage;
+        private String gpu;
+
+        // 객체 생성자 public
+        // cpu 필드만 매개변수 받음 (cpu 필드 필수값)
+        public ComputerBuilder(String cpu) {
+            this.cpu = cpu;
+        }
+
+        // cpu 를 제외한 나머지 필드에 대한 setter
+        // - this (자기자신, computer builder) 를 반환
+        // -> 메서드 체이닝 가능
+        public ComputerBuilder setRam(int ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        public ComputerBuilder setStorage(int storage) {
+            this.storage = storage;
+            return this;
+        }
+
+        public ComputerBuilder setGpu(String gpu) {
+            this.gpu = gpu;
+            return this;
+        }
+
+        // 최종적으로 객체 생성
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram=" + ram + "GB" +
+                ", storage=" + storage + "GB" +
+                ", gpu='" + gpu + '\'' +
+                '}';
+    }
 }
--- java-language/src/_10_others/_builder/Main.java
+++ java-language/src/_10_others/_builder/Main.java
@@ -1,4 +1,37 @@
 package _10_others._builder;
 
 public class Main {
+    public static void main(String[] args) {
+        // 빌더 패턴을 안쓰고 생성자를 통해 객체 생성시 예시
+        // Student std = new Student("John", 10, 1, "LA");
+
+        Computer basicComputer = new Computer.ComputerBuilder("Intel i5")
+                .setRam(8)
+                .setStorage(256)
+                .build();
+        System.out.println("basicComputer = " + basicComputer);
+
+        Computer gamingComputer = new Computer.ComputerBuilder("AMD Ryzen 7")
+                .setStorage(1000)
+                .setRam(32)
+                .setGpu("NVIDIA RTX 3080")
+                .build();
+        System.out.println("gamingComputer = " + gamingComputer);
+
+        Computer superComputer = new Computer.ComputerBuilder("Intel i7")
+                .setRam(16)
+                .setStorage(256)
+                .build();
+        System.out.println("superComputer = " + superComputer);
+    }
 }
+
+// Builder 패턴
+// - 객체 생성을 직관적이고 안전하게 만들어주는 디자인 패턴
+// - Lombok 라이브러리의 @Builder 어노테이션을 이용하면 자동으로 Builder 클래스를 생성해줌
+
+// 장점
+// 1. 가독성 향상
+// 2. 유연성: 필요한 데이터만 선택적으로 설정
+// 3. 불변성: 객체 생성 후에 변경할 수 없게 만듦
+// 4. 매개변수 순서 문제 해결: 생성자의 매개변수 순서를 기억할 필요가 없다!
